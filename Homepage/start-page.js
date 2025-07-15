a5add751571a3ddfda31e08a4d3c176831b8abbf
ProProgrammer1234 <127347148+ProProgrammer1234@users.noreply.github.com>
2025-07-15 18:13:04 +0300

    Add files via upload

---
+++ Homepage/start-page.js
@@ -0,0 +1,24 @@
+let username = document.getElementById("username")
+let button = document.getElementById("btn")
+
+// console.log(username);
+// console.log(button);
+
+function roulette() {
+    if(username.value.trim().length > 0) {
+        localStorage.setItem("Username", username.value)
+        console.log(username.value);
+        location.href = "index.html"
+    }
+}
+
+function higherOrLower() {
+    if(username.value.trim().length > 0) {
+        localStorage.setItem("Username", username.value)
+        console.log(username.value);
+        location.href = "game2.html"
+    }
+}
+
+
+
